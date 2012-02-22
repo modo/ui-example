49d1402787537f0d180609069ac8db741fb441e8
Travis Lehman <trav.lehman@gmail.com>
2012-02-22 15:44:54 -0500

    docs for tables

--- lib/docs/tables.php
+++ lib/docs/tables.php
@@ -1,123 +1,201 @@
 <h1 class="section-header">Tables</h1>			            	
-<h2>Table</h2>
-   <table cellspacing="0" class="">
-	<thead class="small-text">
+
+<div class="row">
+    <div class="span4">
+		<h4>Minimal table</h4>
+<pre class="prettyprint linenums">
+<? cleanup_code('<table>
+	…
+</table>') ?>
+</pre>
+    </div>
+	<div class="span6">
+		<table>
+		<thead>
 		<tr>
-			<td></td>
-			<th scope="col">Base Pay</th>
-			<th scope="col">Travel Stipend</th>
-			<th scope="col">TOTAL</th>
+		<th>#</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Country</th>
 		</tr>
-	</thead>
-	<tbody class="smaller-text"> 
+		</thead>
+		<tbody>
 		<tr>
-			<th scope="row">4-hour</th>
-			<td>$950</td>
-			<td>$750</td>
-			<td>$1,700</td>
-		</tr>
-		<tr>
-			<th scope="row">8-hour</th>
-			<td>$1,450</td>
-			<td>$750</td>
-			<td>$2,200</td>
+		<td>1</td>
+		<td>Audi</td>
+		<td>R8</td>
+		<td>Germany</td>
 		</tr>
 		<tr>
-			<th scope="row">2-day</th>
-			<td>$2,900</td>
-			<td>$970</td>
-			<td>$3,870</td>
+		<td>2</td>
+		<td>Nissan</td>
+		<td>GT-R</td>
+		<td>Japan</td>
 		</tr>
-	</tbody>
-</table>
+		<tr>
+		<td>3</td>
+		<td>Cheverolet</td>
+		<td>Camaro ZL1</td>
+		<td>USA</td>
+		</tr>
+		</tbody>
+		</table>
+	</div>
+</div>
+
+<hr class="white-space" />
+
+<div class="row">
+    <div class="span4">
+		<h4>Minimal table with borders</h4>
+<pre class="prettyprint linenums">
+<? cleanup_code('<table class="table-bordered">
+	…
+</table>') ?>
+</pre>
+    </div>
+	<div class="span6">
+		<table class="table-bordered">
+		<thead>
+		<tr>
+		<th>#</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Country</th>
+		</tr>
+		</thead>
+		<tbody>
+		<tr>
+		<td>1</td>
+		<td>Audi</td>
+		<td>R8</td>
+		<td>Germany</td>
+		</tr>
+		<tr>
+		<td>2</td>
+		<td>Nissan</td>
+		<td>GT-R</td>
+		<td>Japan</td>
+		</tr>
+		<tr>
+		<td>3</td>
+		<td>Cheverolet</td>
+		<td>Camaro ZL1</td>
+		<td>USA</td>
+		</tr>
+		</tbody>
+		</table>
+	</div>
+</div>
 
 
-<h2>Table + <code>.table-bordered</code></h2>
-<table cellspacing="0" class="table-bordered">
-	<thead class="small-text">
+
+<hr class="white-space" />
+
+<div class="row">
+    <div class="span4">
+		<h4>Stylized table</h4>
+<pre class="prettyprint linenums">
+<? cleanup_code('<table class="table-stylized">
+	…
+</table>') ?>
+</pre>
+    </div>
+	<div class="span6">
+		<table class="table-stylized">
+		<thead>
 		<tr>
-			<td></td>
-			<th scope="col">Base Pay</th>
-			<th scope="col">Travel Stipend</th>
-			<th scope="col">TOTAL</th>
+		<th>#</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Country</th>
 		</tr>
-	</thead>
-	<tbody class="smaller-text">
+		</thead>
+		<tbody>
 		<tr>
-			<th scope="row">4-hour</th>
-			<td>$950</td>
-			<td>$750</td>
-			<td>$1,700</td>
-		</tr>
-		<tr>
-			<th scope="row">8-hour</th>
-			<td>$1,450</td>
-			<td>$750</td>
-			<td>$2,200</td>
+		<td>1</td>
+		<td>Audi</td>
+		<td>R8</td>
+		<td>Germany</td>
 		</tr>
 		<tr>
-			<th scope="row">2-day</th>
-			<td>$2,900</td>
-			<td>$970</td>
-			<td>$3,870</td>
+		<td>2</td>
+		<td>Nissan</td>
+		<td>GT-R</td>
+		<td>Japan</td>
 		</tr>
-	</tbody>
-</table>
+		<tr>
+		<td>3</td>
+		<td>Cheverolet</td>
+		<td>Camaro ZL1</td>
+		<td>USA</td>
+		</tr>
+		</tbody>
+		</table>
+	</div>
+</div>
+
+<hr class="white-space" />
 
 
-<h2>Table + <code>.table-fancy</code></h2>
-<table cellspacing="0" class="table-fancy ">
-	<thead class="small-text">
+<div class="row">
+    <div class="span4">
+		<h4>Zebra-striped table</h4>
+<pre class="prettyprint linenums">
+<? cleanup_code('<table class="table-stylized">
+	…
+</table>') ?>
+</pre>
+		<p class="small-text"><strong>Note:</strong> This demo also uses the <code>.small-text</code> &amp; <code>.smaller-text</code> classes to adjust font size.</p>
+    </div>
+	<div class="span6">
+		<table class="table-bordered table-striped">
+		<thead class="small-text">
 		<tr>
-			<td></td>
-			<th scope="col">Base Pay</th>
-			<th scope="col">Travel Stipend</th>
-			<th scope="col">TOTAL</th>
+		<th>#</th>
+		<th>Make</th>
+		<th>Model</th>
+		<th>Country</th>
 		</tr>
-	</thead>
-	<tbody class="smaller-text">
+		</thead>
+		<tbody class="smaller-text">
 		<tr>
-			<th scope="row">4-hour</th>
-			<td>$950</td>
-			<td>$750</td>
-			<td>$1,700</td>
-		</tr>
-		<tr>
-			<th scope="row">8-hour</th>
-			<td>$1,450</td>
-			<td>$750</td>
-			<td>$2,200</td>
-		</tr>
-		<tr>
-			<th scope="row">2-day</th>
-			<td>$2,900</td>
-			<td>$970</td>
-			<td>$3,870</td>
-		</tr>
-	</tbody>
-</table>
-
-
-<h2>Table + <code>.table-fancy</code></h2>
-<table cellspacing="0" class="table-fancy ">
-	<tbody class="smaller-text">
-		<tr>
-			<th class="span2" scope="row">4-hour</th>
-			<td class="span2">$950</td>
-			<td class="span2">$750</td>
-			<td class="span2">$1,700</td>
+		<td>1</td>
+		<td>Audi</td>
+		<td>R8</td>
+		<td>Germany</td>
 		</tr>
 		<tr>
-			<th scope="row">8-hour</th>
-			<td>$1,450</td>
-			<td>$750</td>
-			<td>$2,200</td>
+		<td>2</td>
+		<td>Nissan</td>
+		<td>GT-R</td>
+		<td>Japan</td>
 		</tr>
 		<tr>
-			<th scope="row">2-day</th>
-			<td>$2,900</td>
-			<td>$970</td>
-			<td>$3,870</td>
+		<td>3</td>
+		<td>Cheverolet</td>
+		<td>Corvette</td>
+		<td>USA</td>
 		</tr>
-	</tbody>
-</table>
+		<tr>
+		<td>3</td>
+		<td>lamborghini</td>
+		<td>Gallardo</td>
+		<td>Italy</td>
+		</tr>
+		<tr>
+		<td>3</td>
+		<td>Ford</td>
+		<td>GT</td>
+		<td>USA</td>
+		</tr>
+		<tr>
+		<td>3</td>
+		<td>Aston Martin</td>
+		<td>V8</td>
+		<td>UK</td>
+		</tr>
+		</tbody>
+		</table>
+	</div>
+</div>
