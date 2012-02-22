86aefd525c0c86e37170b2b26680b889168d5540
Josh Lasdin <josh@mododesigngroup.com>
2012-02-22 10:38:16 -0500

    Pictos link

--- lib/inc/meta-head.php
+++ lib/inc/meta-head.php
@@ -1,6 +1,5 @@
 <link rel="stylesheet" href="/lib/css/styles.css" />
-
-<link href="//get.pictos.cc/fonts/734/9" rel="stylesheet" type="text/css">		<!-- Pictos Arrows -->
+<link href="//get.pictos.cc/fonts/734/13" rel="stylesheet" type="text/css">		<!-- Pictos Arrows -->
 
 <script src="http://use.typekit.com/rsm7gxu.js"></script>
 <script src="/lib/js/min/preload-min.js"></script>
