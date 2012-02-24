48d3fe7b64ed12fb958be217781a97f282172981
Josh Lasdin <josh@mododesigngroup.com>
2012-02-24 14:26:21 -0500

    Section head documentation

--- lib/docs/boxes.php
+++ lib/docs/boxes.php
@@ -82,15 +82,47 @@
 			<p class="">Etiam sagittis elementum libero quis fermentum urabitur aliquet pretium. </p>
 		</div>
     </div>
-
-
-
-
-
-
-
 </div>
 
+<hr class="thick-rule">
 
+<h2>Section head</h2>
+<div class="section-head">
+	<h3 class="pull-left">Welcome, John Smith!</h3>
+	<div class="pull-right">
+		<p class="small-text"><strong>Member ID #4445875</strong></p>
+	</div>
+</div>
 
-
+<div class="row">
+	<div class="span4">
+		<div class="box">
+			<p>Section headers are used sparingly, only when it is necessary to anchor the header of a page. Color options are the same as buttons.</p>
+			<pre class="prettyprint linenums"><? cleanup_code('<div class="section-head">…</div>') ?></pre>
+		</div>
+	</div>
+	<div class="span6">
+		<div class="row">
+			<div class="span3">
+				<p><code>.section-head-info</code></p>
+				<div class="section-head section-head-info"><h5>Lorem ipsum dolor sit amet</h5></div>
+				
+				<p><code>.section-head-dark</code></p>
+				<div class="section-head section-head-dark"><h5>Lorem ipsum dolor sit amet</h5></div>
+				
+				<p><code>.section-head-primary</code></p>
+				<div class="section-head section-head-primary"><h5>Lorem ipsum dolor sit amet</h5></div>
+			</div>
+			<div class="span3">
+				<p><code>.section-head-warning</code></p>
+				<div class="section-head section-head-warning"><h5>Lorem ipsum dolor sit amet</h5></div>
+				
+				<p><code>.section-head-danger</code></p>
+				<div class="section-head section-head-danger"><h5>Lorem ipsum dolor sit amet</h5></div>
+				
+				<p><code>.section-head-success</code></p>
+				<div class="section-head section-head-success"><h5>Lorem ipsum dolor sit amet</h5></div>
+			</div>
+		</div>
+	</div>
+</div>
