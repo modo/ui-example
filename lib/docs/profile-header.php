938c7338530afef3531d70104e56a3a178ead777
Josh Lasdin <josh@mododesigngroup.com>
2012-03-21 17:17:12 -0400

    Profile header documentation

---
+++ lib/docs/profile-header.php
@@ -0,0 +1,60 @@
+<h1 class="section-header">Profile Header</h1>
+<header class="profile-head">
+	<div class="well">
+		<div class="row top">
+			<div class="span2">
+				<div class="frame-container">
+		            <div class="frame bordered">
+		                <img src="http://f.cl.ly/items/1I15120S0g0P3m404746/Image%202012.03.21%202:11:06%20PM.png" alt="" />
+		            </div>
+		        </div>
+			</div>
+			<div class="span8">
+				<h1 class="section-header header-2">John Adams</h1>
+				<h2 class="header-5">General Manager, <a href="#">Georgia Institute of Technology</a></h2>
+				
+				<div class="row">
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li>760 Spring Street<br />
+							Suite 330<br />
+							Mableton, GA 30332-0640</li>
+							<li><a href="#"><i class="icon-map-marker icon-black"></i> Map</a></li>
+						</ul>
+					</div>
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li>703-123-1234</li>
+						</ul>
+					</div>
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li><a href="#">Visit website</a></li>
+							<li><a href="#"><i class="icon-envelope icon-black"></i> jadams@greenuguard.org</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="row">
+			<nav class="span7">
+				<ul class="nav nav-pills">
+					<li class="active"><a href="#">Overview</a></li>
+					<li><a href="#">Employees</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Activity</a></li>
+				</ul>
+			</nav>
+			<div class="span3">
+				<div class="btn-row pull-right">
+					<p>Follow on</p>
+					<i class="icon-social-twitter icon-med"></i>
+					<i class="icon-social-facebook icon-med"></i>
+					<i class="icon-social-linked-in icon-med"></i>
+				</div>
+			</div>
+		</div>
+	</div>
+</header>
