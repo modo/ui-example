7c8e3920d5fed1a6d349b204321480d8a106e4df
Josh Lasdin <josh@mododesigngroup.com>
2012-03-22 10:35:40 -0400

    Profile header documentation

--- lib/docs/profile-header.php
+++ lib/docs/profile-header.php
@@ -1,5 +1,4 @@
 <h1 class="section-header">Profile Header</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a tellus orci. Suspendisse at cursus diam. Vivamus eu venenatis urna. Nam cursus lacinia enim non mattis. Aliquam fermentum nisi at turpis consequat sit amet semper ante consectetur. Donec nulla sem, ultricies quis malesuada quis, feugiat a dolor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc vulputate, massa ut eleifend consectetur, risus nunc eleifend orci, vel convallis mauris velit eget purus. Proin interdum tellus sed enim viverra ut commodo lectus tincidunt. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam ac lacus nunc, sed consequat eros. Nunc interdum molestie felis quis hendrerit. Maecenas lacus leo, tempus sed eleifend ut, tempus id enim. Cras bibendum ante vel arcu tempus iaculis. Nulla facilisi. Suspendisse nisl neque, adipiscing et faucibus nec, aliquam ac erat. Morbi erat velit, posuere eu bibendum nec, gravida quis nunc. Nullam condimentum, mauris at commodo pharetra, nisl ipsum rutrum massa, vel vehicula sem sapien ut lorem.</p>
 <header class="profile-head">
 	<div class="well">
 		<div class="row top">
@@ -13,6 +12,11 @@
 			<div class="span8">
 				<h1 class="section-header header-2">John Adams</h1>
 				<h2 class="header-5">General Manager, <a href="#">Georgia Institute of Technology</a></h2>
+				
+				<div class="credentials">
+					<img alt="LEED AP BD+C" title="John Adams is a LEED Accredited Professional with Building Design &amp; Construction Specialty (LEED AP BD+C)" src="http://phase2.usgbc.trav.mododev.com/themes/usgbc/lib/img/cred-icons/credential-ap-bdc.gif" />
+					<img alt="LEED AP HOMES" title="John Adams is a LEED Accredited Professional with Homes Specialty (LEED AP Homes)" src="http://phase2.usgbc.trav.mododev.com/themes/usgbc/lib/img/cred-icons/credential-ap-homes.gif" />                   
+				</div>
 				
 				<div class="row">
 					<div class="span2">
@@ -51,11 +55,74 @@
 			<div class="span3">
 				<div class="btn-row pull-right">
 					<p>Follow on</p>
-					<i class="icon-social-twitter icon-med"></i>
-					<i class="icon-social-facebook icon-med"></i>
-					<i class="icon-social-linked-in icon-med"></i>
+					<a href="#"><i class="icon-social-twitter icon-med"></i></a>
+					<a href="#"><i class="icon-social-facebook icon-med"></i></a>
+					<a href="#"><i class="icon-social-linked-in icon-med"></i></a>
 				</div>
 			</div>
 		</div>
 	</div>
 </header>
+
+<pre class="prettyprint linenums">
+<? cleanup_code('<header class="profile-head">
+	<div class="well">
+		<div class="row top">
+			<div class="span2">
+				<div class="frame-container">
+		            <div class="frame bordered">
+		                <img src="…" alt="" />
+		            </div>
+		        </div>
+			</div>
+			<div class="span8">
+				<h1 class="section-header header-2"><!-- Name --></h1>
+				<h2 class="header-5"><!-- Sub line (Title, Organization, etc) --></h2>
+				
+				<div class="credentials"> <!-- (if needed) -->
+					<img alt="…" title="…" src="…" />                   
+				</div>
+				
+				<div class="row">
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li><!-- Address --><br />
+							…<br />
+							…</li>
+							<li><a href="#"><i class="icon-map-marker icon-black"></i> Map</a></li>
+						</ul>
+					</div>
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li><!-- Phone --></li>
+						</ul>
+					</div>
+					<div class="span2">
+						<ul class="unstyled smaller-text">
+							<li><a href="#">Visit website</a></li>
+							<li><a href="#"><i class="icon-envelope icon-black"></i> <!-- Email --></a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="bottom">
+		<div class="row">
+			<nav class="span7">
+				<ul class="nav nav-pills">
+					<li><a href="#">Overview</a></li>
+					…
+				</ul>
+			</nav>
+			<div class="span3">
+				<div class="btn-row pull-right">
+					<p>Follow on</p>
+					<a href="#"><i class="icon-social-twitter icon-med"></i></a>
+					…
+				</div>
+			</div>
+		</div>
+	</div>
+</header>') ?>
+</pre>
