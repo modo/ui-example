903c39c45a68e6c1875b84f0f30dfd6bd0b14660
Josh Lasdin <josh@mododesigngroup.com>
2012-03-22 16:09:32 -0400

    Commenting documentation

--- components.php
+++ components.php
@@ -34,6 +34,7 @@ $level2 = 'components';
 			            	<section><?php include('lib/docs/rating-widgets.php') ?></section>
 			            	<section><?php include('lib/docs/alert-list.php') ?></section>
 			            	<section><?php include('lib/docs/profile-header.php') ?></section>
+			            	<section><?php include('lib/docs/commenting.php') ?></section>
 			            </article>			            
 					</div>
 				</div>
